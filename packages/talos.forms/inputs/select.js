163b8f60973c15367584d12ecbf5a0a134fba3d0
Raphael Arias <raphael@talos.email>
2017-01-27 13:28:11 +1100

    Fix select input

--- packages/talos.forms/inputs/select.js
+++ packages/talos.forms/inputs/select.js
@@ -2,71 +2,80 @@
 
 import { Template } from 'meteor/templating';
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
+import $ from 'jquery';
 
 checkNpmVersions({
-  jquery: '2.2.4',
+  jquery: '3.1.1',
 }, 'talos:forms');
 
-const $ = require('jquery');
-
-const getmdlSelect = {
-  defaultValue: {
-    width: 300,
-  },
-  addEventListeners(dropdown) {
-    const input = dropdown.querySelector('input');
-    const list = dropdown.querySelectorAll('li');
-    const menu = dropdown.querySelector('.mdl-js-menu');
-
-    // show menu on mouse down or mouse up
-    input.onkeydown = function (event) {
-      if (event.keyCode == 38 || event.keyCode == 40) {
-        menu['MaterialMenu'].show();
-      }
-    };
-
-    // return focus to input
-    menu.onkeydown = function (event) {
-      if (event.keyCode == 13) {
-        input.focus();
-      }
-    };
-
-    [].forEach.call(list, function (li) {
-      li.onclick = function () {
-        input.value = li.textContent;
-        dropdown.MaterialTextfield.change(li.textContent); // handles css class changes
-        setTimeout(function () {
-          dropdown.MaterialTextfield.updateClasses_(); // update css class
-        }, 250);
-
-        // update input with the "id" value
-        input.dataset.val = li.dataset.val || '';
-
-        if ("createEvent" in document) {
-          var evt = document.createEvent("HTMLEvents");
-          evt.initEvent("change", false, true);
-          input.dispatchEvent(evt);
-        } else {
-          input.fireEvent("onchange");
-        }
-      };
-    });
-  },
-  init(selector, widthDef) {
-    const dropdowns = document.querySelectorAll(selector);
-    [].forEach.call(dropdowns, (i) => {
-      getmdlSelect.addEventListeners(i);
-      const width = widthDef ? widthDef : (i.querySelector('.mdl-menu').offsetWidth ? i.querySelector('.mdl-menu').offsetWidth : getmdlSelect.defaultValue.width);
-      i.style.width = width + 'px';
-    });
-  },
-};
+ReactiveForms.createElement({
+  template: 'selectInput',
+  validationEvent: 'change',
+  validationValue(el, clean, template) {
+    return $(el).attr('data-val');
+  }
+});
 
 Template.selectInput.onRendered(function () {
+  const getmdlSelect = {
+    defaultValue: {
+      width: 300,
+    },
+    addEventListeners(dropdown) {
+      const input = dropdown.querySelector('input');
+      const list = dropdown.querySelectorAll('li');
+      const menu = dropdown.querySelector('.mdl-js-menu');
+
+      // show menu on mouse down or mouse up
+      input.onkeydown = function (event) {
+        if (event.keyCode == 38 || event.keyCode == 40) {
+          menu['MaterialMenu'].show();
+        }
+      };
+
+      // return focus to input
+      if (!menu) return;
+      menu.onkeydown = function (event) {
+        if (event.keyCode == 13) {
+          input.focus();
+        }
+      };
+
+      [].forEach.call(list, function (li) {
+        li.onclick = function () {
+          input.value = li.textContent;
+          dropdown.MaterialTextfield.change(li.textContent); // handles css class changes
+          setTimeout(function () {
+            dropdown.MaterialTextfield.updateClasses_(); // update css class
+          }, 250);
+
+          // update input with the "id" value
+          input.dataset.val = li.dataset.val || '';
+
+          if ("createEvent" in document) {
+            var evt = document.createEvent("HTMLEvents");
+            evt.initEvent("change", false, true);
+            input.dispatchEvent(evt);
+          } else {
+            input.fireEvent("onchange");
+          }
+        };
+      });
+    },
+    init(selector, widthDef) {
+      const dropdowns = document.querySelectorAll(selector);
+
+      [].forEach.call(dropdowns, (i) => {
+        getmdlSelect.addEventListeners(i);
+        const width = widthDef ? widthDef : (i.querySelector('.mdl-menu').offsetWidth ? i.querySelector('.mdl-menu').offsetWidth : getmdlSelect.defaultValue.width);
+        i.style.width = width + 'px';
+      });
+    },
+  };
+
   getmdlSelect.init('.getmdl-select');
-  document.addEventListener("DOMNodeInserted", function (ev) {
-    if (ev.relatedNode.querySelectorAll(".getmdl-select").length > 0) {
+  document.addEventListener('DOMNodeInserted', function (ev) {
+    if (ev.relatedNode.querySelectorAll('.getmdl-select').length > 0) {
       componentHandler.upgradeDom();
     }
   }, false);
@@ -80,6 +89,7 @@ Template.selectInput.events({
 
     // Emit change event
     $selectInput.trigger('change');
+    template.$('.reactive-element').trigger('change');
   },
   'focus .js-select-input'(e) {
     $(e.currentTarget)
