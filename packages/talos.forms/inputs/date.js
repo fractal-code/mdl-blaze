b7efd5ca16f4d41ff4411322d3b6eca5c16974a8
Raphael Arias <raphael@talos.email>
2017-01-27 12:31:24 +1100

    Fix input date

--- packages/talos.forms/inputs/date.js
+++ packages/talos.forms/inputs/date.js
@@ -3,49 +3,58 @@
 import { Template } from 'meteor/templating';
 import { ReactiveForms } from 'meteor/templates:forms';
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
-import { Tracker } from 'meteor/tracker';
+import $ from 'jquery';
 
 checkNpmVersions({
   'md-date-time-picker': '2.2.0',
 }, 'talos:forms');
 
 checkNpmVersions({
-  jquery: '2.2.4',
+  jquery: '3.1.1',
 }, 'talos:forms');
 
 const mdDateTimePicker = require('md-date-time-picker');
-const $ = require('jquery');
+
 
 ReactiveForms.createElement({
   template: 'dateInput',
-  validationEvent: 'input',
+  validationEvent: 'change',
+  validationValue(el, clean, template) {
+    return new Date($(el).attr('data-value'));
+  }
 });
 
-Template.dateInput.onRendered(() => {
+Template.dateInput.onRendered(function () {
+  const $reactiveElement = this.$('.reactive-element');
+
   const timeOptions = Template.currentData().options;
 
   if (!timeOptions.mdDateTimePicker.type) timeOptions.mdDateTimePicker.type = 'date';
-  // Create new Date dialog
   const dialog = new mdDateTimePicker.default(timeOptions.mdDateTimePicker);
 
-  // Get field options
-  Tracker.autorun(() => {
+  this.autorun(() => {
     // If options exist:
     // 1 - Add the event handler to toggle the dialog
     // 2 - Attach the input to the trigger (for the onOk to work)
     // 3 - Add event onOk to get the date
     if (timeOptions) {
-      $(timeOptions.element).click(() => {
+      this.$(timeOptions.element).click(() => {
         dialog.toggle();
       });
 
-      dialog.trigger = $(timeOptions.element)[0];
+      dialog.trigger = this.$(timeOptions.element)[0];
 
       $(timeOptions.element).on('onOk', (event) => {
-        $(event.target)
+        this.$(event.target)
           .val(dialog.time.format(timeOptions.moment.format))
           .parent()
           .addClass('is-dirty');
+
+        // Save date
+        // Trigger 'change' event handler for reactive element
+        $reactiveElement
+          .attr('data-value', dialog.time.toDate())
+          .trigger('change');
       });
     }
   });
